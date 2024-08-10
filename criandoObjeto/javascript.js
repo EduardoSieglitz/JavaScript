883b12126ecc35514e5456dee3ad4b135a76fc35
EduardoSieglitz <eduardosieglitzdasilveira@gmail.com>
2024-08-09 22:47:09 -0300

    Objeto para Banco de Dados
    -Criando uma lista de objeto, listando alguns atributos para cada pessoa. Atividade básica de JavaScript.

---
+++ criandoObjeto/javascript.js
@@ -0,0 +1,38 @@
+
+const Pessoas = [{
+    id: 1,
+    nome: "João",
+    idade: 30,
+    altura: 1.80,
+    CPF: "111.111.111-11"
+},
+{
+    id: 2,
+    nome: "Eduardo",
+    idade: 18,
+    altura: 1.86,
+    CPF: "222.111.222-11"
+},
+{
+    id: 3,
+    nome: "Pedro",
+    idade: 62,
+    altura: 1.70,
+    CPF: "131.333.131-33"
+},
+{
+    id: 4,
+    nome: "Diogo",
+    idade: 31,
+    altura: 1.90,
+    CPF: "666.621.441-51"
+},
+{
+    id: 5,
+    nome: "Diego",
+    idade: 40,
+    altura: 1.64,
+    CPF: "999.911.191-19"
+}
+];
+console.log(Pessoas);
